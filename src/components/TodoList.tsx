a3212171ea1a8ba080c97b0e4ea58e8d1f1c4c28
cristhianprog <cristhianprog@gmail.com>
2025-05-26 14:11:06 -0300

    insert input search

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,6 @@
-import { List, Spin } from 'antd';
+import { Input, List, Spin } from 'antd';
 import styled from 'styled-components';
+import { useState } from 'react';
 import { TodoEntry } from '../types/todo';
 import { TodoItem } from './TodoItem';
 
@@ -12,38 +13,55 @@ type Props = {
 };
 
 export const TodoList = ({ todos, loading, onToggle, onRemove, onEdit }: Props) => {
+  const [search, setSearch] = useState('');
+
   if (loading) {
     return <Spin />;
   }
 
-  const orderedTodos = [...todos].sort((a, b) => {
+  const filteredTodos = todos.filter(
+    (todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase()) ||
+      todo.id.toString().includes(search)
+  );
+
+  const orderedTodos = [...filteredTodos].sort((a, b) => {
     if (a.checked === b.checked) {
-      return  b.id - a.id;
+      return b.id - a.id;
     }
     return Number(a.checked) - Number(b.checked);
   });
 
   return (
-    <StyledList
-      bordered
-      dataSource={orderedTodos}
-      renderItem={(item) => (
-        <TodoItem
-          key={item.id}
-          id={item.id}
-          content={item.content}
-          checked={item.checked}
-          onToggle={onToggle}
-          onRemove={onRemove}
-          onEdit={onEdit}
-        />
-      )}
-    />
+    <div>
+      <Input
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        style={{ marginBottom: 16 }}
+      />
+
+      <StyledList
+        bordered
+        dataSource={orderedTodos}
+        renderItem={(item) => (
+          <TodoItem
+            key={item.id}
+            id={item.id}
+            content={item.content}
+            checked={item.checked}
+            onToggle={onToggle}
+            onRemove={onRemove}
+            onEdit={onEdit}
+          />
+        )}
+      />
+    </div>
   );
 };
 
 const StyledList = styled(List<TodoEntry>)`
-  background-color:#eafffc;
+  background-color: #eafffc;
   border-radius: 8px;
   padding: 16px;
 
